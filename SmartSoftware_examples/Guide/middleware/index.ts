265c47dc008fa15766e85c540c33bfbd8b07ab1c
frontdevjs <front_dev_js@mail.ru>
2023-09-29 14:35:49 +0500

    refactoring middleware

--- SmartSoftware_examples/Guide/middleware/index.ts
+++ SmartSoftware_examples/Guide/middleware/index.ts
@@ -1,20 +1,33 @@
-import {Bot} from "https://deno.land/x/grammy@v1.19.0/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.19.0/mod.ts";
+// import {Composer} from "https://deno.land/x/grammy@v1.18.1/composer.ts";
+import {Composer} from "https://deno.land/x/grammy@v1.18.1/mod.ts";
+
 const bot = new Bot("6512809429:AAEugT-aHuLgGOitPnHlOS0y6dZKqjWbFA0");
-import middleware from './middleware.ts'
 
+async function responseTime(
+    ctx: Context,
+    next: NextFunction, // is an alias for: () => Promise<void>
+): Promise<void> {
+    // take time before
+    const before = Date.now(); // milliseconds
+    // invoke downstream middleware
+    await next(); // make sure to `await`!
+    // take time after
+    const after = Date.now(); // milliseconds
+    // log difference
+    console.log(`Response time: ${after - before} ms`);
+    ctx.reply(`Response time: ${after - before} ms`)
+    ctx.reply(`message_id: ${ctx.msg.message_id}`)
+}
 
-bot.use(middleware());
-
-bot.command("start", middleware);
+// bot.use(responseTime);
+const composer = new Composer();
+composer.use(responseTime)
+bot.use(composer)
+bot.command("start", (ctx) => ctx.reply("Started!"));
 bot.command("help", (ctx) => ctx.reply("Help text"));
 
 bot.on(":text", (ctx) => ctx.reply("Text!")); // (*)
 bot.on(":photo", (ctx) => ctx.reply("Photo!"));
 
-
-
-
 bot.start();
-
-
-
